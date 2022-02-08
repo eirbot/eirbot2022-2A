c153c853134b49789eb5992a3610eaed5bdb676a
DIEUDONNE Clara <cdieudonn001@enseirb-matmeca.fr>
2022-02-08 19:39:18 +0100

    modification précision tableau table

--- info/table.cpp
+++ info/table.cpp
@@ -1,9 +1,26 @@
 #include "table.hpp"
+#include <cstdlib>
 #include <cstdio>
 
-Table::Table(int s)
+Table::Table(int s, int h, int w)
 {
     scale = s;
+    height = h;
+    width = w;
+    table = (int **)malloc(height * sizeof(int *));
+    for (int i = 0; i < height; i++)
+    {
+        table[i] = (int *)malloc(width * sizeof(int));
+    }
+}
+
+void Table::lib()
+{
+    for (int i = 0; i < height; i++)
+    {
+        free(table[i]);
+    }
+    free(table);
 }
 
 // vide : 0
@@ -13,31 +30,30 @@ Table::Table(int s)
 // robot enemie : 4
 void Table::fill()
 {
-    for (int i = 0; i < 150; i++)
+    for (int i = 0; i < 1500; i++)
     {
-        for (int j = 0; j < 200; j++)
+        for (int j = 0; j < height; j++)
         {
             int val = 0;
             // Gauche
-            if (i <= 10 && j >= 117 && j <= 132)
+            if (i <= 102 && j >= 1175 && j <= 1325)
             {
                 val = 1;
             }
-            // coeff y = (300-149)/51 * x + 149
-            if (j >= ((200 - 149) / (51)) * i + 149)
+            if (j >= ((height - 1490) / (510)) * i + 1490)
             {
                 val = 1;
             }
-            if (j <= 8 && i >= 45 && i <= 117)
+            if (j <= 80 && i >= 450 && i <= 1170)
             {
                 val = 1;
             }
-            if (j <= 10 && i >= 127 && i <= 142)
+            if (j <= 100 && i >= 1275 && i <= 1425)
             {
                 val = 1;
             }
-            table[300 - 1 - i][j] = val;
-            table[i][j] = val;
+            table[j][width - 1 - i] = val;
+            table[j][i] = val;
         }
     }
 }
@@ -46,35 +62,35 @@ void Table::mouv()
 {
     for (int i = 0; i < 6; i++)
     {
-        for (int j = 0; j <= 6; j ++)
+        for (int j = 0; j <= 60; j++)
         {
-            for (int k = 0; k <= 7; k ++)
+            for (int k = 0; k <= 70; k++)
             {
-                table[palets[i][0] - j][palets[i][1] - k] = 2;
-                table[palets[i][0] + j][palets[i][1] + k] = 2;
-                table[palets[i][0] - j][palets[i][1] + k] = 2;
-                table[palets[i][0] + j][palets[i][1] - k] = 2;
+                table[palets[i][1] - k][palets[i][0] - j] = 2;
+                table[palets[i][1] + k][palets[i][0] + j] = 2;
+                table[palets[i][1] + k][palets[i][0] - j] = 2;
+                table[palets[i][1] - k][palets[i][0] + j] = 2;
             }
         }
     }
-    for (int j = 0; j <= 15; j ++)
+    for (int j = 0; j <= 150; j++)
     {
-        for (int k = 0; k <= 15; k ++)
+        for (int k = 0; k <= 150; k++)
         {
-            table[my_robot[0] - j][my_robot[1] - k] = 3;
-            table[my_robot[0] + j][my_robot[1] + k] = 3;
-            table[my_robot[0] - j][my_robot[1] + k] = 3;
-            table[my_robot[0] + j][my_robot[1] - k] = 3;
+            table[my_robot[1] - j][my_robot[0] - k] = 3;
+            table[my_robot[1] + j][my_robot[0] + k] = 3;
+            table[my_robot[1] - j][my_robot[0] + k] = 3;
+            table[my_robot[1] + j][my_robot[0] - k] = 3;
         }
     }
-    for (int j = 0; j <= 15; j ++)
+    for (int j = 0; j <= 150; j++)
     {
-        for (int k = 0; k <= 15; k ++)
+        for (int k = 0; k <= 150; k++)
         {
-            table[int(opp_robot[0] - j)][int(opp_robot[1] - k)] = 4;
-            table[int(opp_robot[0] + j)][int(opp_robot[1] + k)] = 4;
-            table[int(opp_robot[0] - j)][int(opp_robot[1] + k)] = 4;
-            table[int(opp_robot[0] + j)][int(opp_robot[1] - k)] = 4;
+            table[int(opp_robot[1] - j)][int(opp_robot[0] - k)] = 4;
+            table[int(opp_robot[1] + j)][int(opp_robot[0] + k)] = 4;
+            table[int(opp_robot[1] - j)][int(opp_robot[0] + k)] = 4;
+            table[int(opp_robot[1] + j)][int(opp_robot[0] - k)] = 4;
         }
     }
 }
@@ -83,12 +99,12 @@ void Table::show()
 {
     fill();
     mouv();
-    for (int j = 0; j < 200; j += scale)
+    for (int j = 0; j < height; j += scale)
     {
-        for (int i = 0; i < 300; i += scale)
+        for (int i = 0; i < width; i += scale)
         {
             const char *s = "";
-            int val = table[i][j];
+            int val = table[j][i];
             if (val == 0)
             {
                 s = "\x1B[34m";
@@ -109,34 +125,37 @@ void Table::show()
             {
                 s = "\x1B[31m";
             }
-            printf("%s%i", s, val);
+            printf("%s%i ", s, val);
         }
         printf("\n");
     }
 }
 
-void Table::set_palets(int x, int y, int pos){
+void Table::set_palets(int x, int y, int pos)
+{
     palets[pos][0] = x;
     palets[pos][1] = y;
 }
 
-void Table::set_my_robot(int x, int y){
+void Table::set_my_robot(int x, int y)
+{
     my_robot[0] = x;
     my_robot[1] = y;
 }
 
-void Table::set_opp_robot(int x, int y){
+void Table::set_opp_robot(int x, int y)
+{
     opp_robot[0] = x;
     opp_robot[1] = y;
 }
 
 int main()
 {
-    Table t(2);
+    Table t(30, 2000, 3000);
     t.show();
-    t.set_my_robot(30,150);
-    t.set_opp_robot(275,60);
-    t.set_palets(89,45,0);
+    t.set_my_robot(300, 1500);
+    t.set_opp_robot(2750, 600);
+    t.set_palets(890, 450, 0);
     t.show();
     return 0;
 }
--- info/table.hpp
+++ info/table.hpp
@@ -1,11 +1,15 @@
 class Table {
-    int table[300][200];
+    int height;
+    int width;
+    int ** table;
     int scale;
-    int palets[6][2] = {{89,55},{83,67},{89,79},{87,127},{105,133},{95,147}};
-    int my_robot[2] = {15,70};
-    int opp_robot[2] = {285,70};
+    int palets[6][2] = {{900,555},{830,675},{900,795},{870,1270},{1050,1330},{950,1470}};
+    int my_robot[2] = {150,700};
+    int opp_robot[2] = {2850,700};
+
     public:
-    Table(int);
+    Table(int, int, int);
+    void lib();
     void fill();
     void mouv();
     void show();
