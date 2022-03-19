f13e6982cb5cd1cbe88655fc4ecfa7eea09daddd
DIEUDONNE Clara <cdieudonn001@enseirb-matmeca.fr>
2022-03-19 18:49:01 +0100

    prise en compte duree

--- info/main.cpp
+++ info/main.cpp
@@ -1,7 +1,14 @@
 #include "Movement.hpp"
 
+#include <time.h> 
+
 int main()
 {
+    float temps;
+    clock_t t1;
+    clock_t t2;
+
+    t1 = clock();
     Table t(10);
     Robot *r2 = new Robot(284, 70, 1);
     Sample *s1 = new Sample(90, 55, 2);
@@ -17,30 +24,41 @@ int main()
     t.addSample(s4);
     t.addSample(s5);
     t.addSample(s6);
+    t2 = clock();
+    temps = (float)(t2-t1)/CLOCKS_PER_SEC;
+    printf("%f\n", temps);
+    t1 = clock();
     t.mouvObstacle();
+    t2 = clock();
+    temps = (float)(t2-t1)/CLOCKS_PER_SEC;
+    printf("%f\n", temps);
+    t1 = clock();
     Node *start = t.nodeAt(15, 15);
     Node *endNode = t.nodeAt(283, 16);
     Node *endNode2 = t.nodeAt(115, 95);
-    start->setPrevious(start);
+    //start->setPrevious(start);
     nodeList end;
     end.push_back(endNode);
     end.push_back(endNode2);
     Astar astar(start, &end, &t, 0);
     nodeList path = astar.findPath();
-    astar.printList(path);
+    //astar.printList(path);
     Movement mov;
     mov.importantPoints(path);
-    astar.printList(mov.getMov());
+    t2 = clock();
+    temps = (float)(t2-t1)/CLOCKS_PER_SEC;
+    printf("%f\n", temps);
+    //astar.printList(mov.getMov());
     int r = 0;
     while(r == 0)
     {
        r = mov.oneStep();
     }
-    astar.printPath(t, &path, astar.getOpen(), astar.getClosed());
+    //astar.printPath(t, &path, astar.getOpen(), astar.getClosed());
     Robot *r1 = new Robot(path.at(path.size() - 1)->getX(), path.at(path.size() - 1)->getY(), 0);
     t.addRobot(r1);
     t.mouvObstacle();
-    t.show(3);
+    //t.show(3);
     delete (r1);
     delete (r2);
     delete (s1);
