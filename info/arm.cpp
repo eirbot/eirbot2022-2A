58e3967a8b30fac622c6e35aca870cb9748773b7
DELMASURE--DHAENE Elias <edelmasured@enseirb-matmeca.fr>
2022-03-25 19:35:12 +0100

    bras

--- info/arm.cpp
+++ info/arm.cpp
@@ -1,5 +1,16 @@
 #include "arm.hpp"
 #include <cstdio>
+#include <cstring>
+
+/*--------------AJOUTER LES FLAGS END_MOVE ET AUTRE SI BESOIN--------------*/
+
+//liste des differentes positions du bras
+//0 = position initiale
+//1 = détecter resistance
+//2 = prendre palets
+//3 = deposer palets
+//4 = prendre statuette
+//5 = deposer statuette
 
 Arm::Arm(int pos_x, int pos_y)
 {
@@ -23,20 +34,27 @@ void Arm::setY(int pos_y) {
     y_arm = pos_y;
 }
 
+//On bouge le bras
 int Arm::move(int idx_position) {
     setX(position[idx_position][0]);
     setY(position[idx_position][1]);
-    printf("[Mode %d] La nouvelle position est %d,%d\n", idx_position, x_arm, y_arm);
+    printf("La nouvelle position est %d,%d\n\n", x_arm, y_arm);
+    return 1;
+}
+
+//On choisit la position du bras
+int Arm::set_pos(Arm a, int idx_position) {
+    printf("Le bras passe en mode %s\n", pos_nom[idx_position]);
+    a.move(idx_position);
     return 1;
 }
 
 int main(int argc, char* argv[]) {
 
     Arm a(2,4);
-    printf("La position est %d,%d\n",a.getX(), a.getY());
-    a.move(0);
-    a.move(2);
-    a.move(4);
+    printf("La position initiale est %d,%d\n\n",a.getX(), a.getY());
+    a.set_pos(a,0);
+    a.set_pos(a,2);
 
     return 0;
 }
--- info/arm.hpp
+++ info/arm.hpp
@@ -1,10 +1,15 @@
 class Arm {
+    char pos_nom[6][5] = {"INIT", "RESD", "PALP", "PALD", "STTP", "STTD"}; //cf differentes pos
     int position[6][2] = {{90,45},{30,70},{45,45},{30,60},{10,10},{40,80}};
     int x_arm;
     int y_arm;
+
     public:
-    Arm(int, int);
-    int move(int);
+    Arm(int, int); //classe bras
+    int move(int); //deplacement du bras
+    int set_pos(Arm, int); //definition de la nouvelle position
+    
+    //getters et setters
     int getX();
     int getY();
     void setX(int);
