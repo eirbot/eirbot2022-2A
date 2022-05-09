4f0478dd19734459a97a2b2805eba31365069431
Sebastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2022-05-09 20:35:35 +0200

    ✨ feat(rasp): first version robot manager

--- com/robot.py
+++ com/robot.py
@@ -2,26 +2,11 @@ import sys
 import requests
 
 #va envoyer une requete http a l adresse ci dessous et la valeur de retour 
-#sera celle de la page pos_robot (server_cam.py ligne 47)
-if(int(sys.argv[1]) == 0):
-    response = requests.get('http://'+sys.argv[2]+':'+sys.argv[3]+'/pos_robot')
-    #print(response.text)
-    
-    str1 = response.text.split(",")
-    pos_fin = [int(str1[0]), int(str1[1])]
-    print("robot")
-#sortie = tableau 1x2 avec les (x,y) du robot
+#sera celle de la page pos_robot (server_cam.py ligne 45)
+response = requests.get('http://'+sys.argv[1]+':'+sys.argv[2]+'/pos_robot')
+#print(response.text)
 
-if(int(sys.argv[1]) == 1):
-    response = requests.get('http://'+sys.argv[2]+':'+sys.argv[3]+'/pos_objet')
-
-    str1 = response.text.split(",")
-    pos_fin = [0]*len(str1)
-    for i in range(len(str1)):
-        pos_fin[i] = int(str1[i])
-    print("objets")
-#sortie = un tableau de taille variable, 1x(2*Nb_objet) avec les (x,y) de chaque objet
-#faire un programme pour le traitement d'un objet parmi ceux detectes, 
-#prendre le plus proche par exemple et éliminer les autres
-
-print(pos_fin)
+#on transforme la chaine de caracteres en tableau
+str1 = response.text.split(",")
+pos=[int(str1[0]),int(str1[1])]
+print(pos)
