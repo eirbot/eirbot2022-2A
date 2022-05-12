ebc806b0c837c4b42da040c290454ffc83289be0
Sebastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2022-05-12 20:16:04 +0200

    ✨ feat(robot_manager): finalize uart communication and gpio initialization

--- info/robot_manager.py
+++ info/robot_manager.py
@@ -7,11 +7,13 @@ import serial
 
 import arm_manager
 
+
 def end_of_world():
     begin = time.time()
-    while time.time() - begin < 10:
+    while time.time() - begin < 80:
         pass
     logging.getLogger().error("ENNNNNNNNNNNNNNNNNND")
+
 
 class RobotManager:
     def __init__(self, simulation: bool = False, baudrate: int = 9600, port: str = "/dev/ttyACM0",
@@ -46,21 +48,31 @@ class RobotManager:
                                           "STOP": "1100"}
         self.type: dict[str, bin] = {"int_16": "0",
                                      "int_32": "1"}
-        self.return_codes: dict[str, bin] = {"KMS": "00000000",
-                                             "RPOOK": "00010000",
-                                             "RROOUT": "00100000",
-                                             "RROOK": "00110000",
-                                             "RPOUT": "01000000", }
-        self.ksm_timeout: int = 60
+        self.return_codes: dict[str, bin] = {"KMS": "\x00",
+                                             "RPOOK": "\x10",
+                                             "RROOUT": "\x20",
+                                             "RROOK": "\x30",
+                                             "RPOUT": "\x40", }
+        self.ksm_timeout: int = 1
         self.move_timeout: int = 10
         logging.getLogger().setLevel(log_level)
         self.initialize_gpio()
         multiprocessing.Process(target=end_of_world).start()
 
+    def serial_read(self):
+        data = self.ser.read()
+        logging.info("Reading from serial: {}".format(data))
+        return data
+
+    def serial_write(self, data: bytes):
+        logging.info("Writing to serial: {}".format(data))
+        self.ser.write(data)
+
     def initialize_gpio(self):
         if self.simulation:
             logging.debug("Initializing GPIO")
             return True
+        from RPi import GPIO
         GPIO.setmode(GPIO.BCM)
         for key in self.GPIO:
             logging.debug("Initializing {} GPIO with pin {} and direction {}".format(key, self.GPIO[key]["pin"],
@@ -75,25 +87,21 @@ class RobotManager:
             arg = "010"
 
         message = self.operation["SPO"] + self.type["int_16"] + arg
-        logging.info(bytes(message, "utf-8"))
         if self.simulation:
             logging.debug("Moving to distance: {}, angle: {}".format(dist, theta))
             return True
 
-        self.ser.write(bytes(message, "utf-8"))
-        self.ser.write(bytes(str(dist), "utf-8"))
-        logging.info(bytes(str(dist), "utf-8"))
+        self.serial_write(bytes(message, "utf-8"))
+        self.serial_write(bytes(str(dist), "utf-8"))
         if theta is not None:
-            self.ser.write(bytes(str(theta), "utf-8"))
-            logging.info(bytes(str(theta), "utf-8"))
+            self.serial_write(bytes(str(theta), "utf-8"))
         self.ser.timeout = self.ksm_timeout
-        output = self.ser.read(size=16)
-        output = bytes(self.return_codes['KMS'], "utf-8")
+        output = self.serial_read()
         if output != bytes(self.return_codes["KMS"], "utf-8"):
             self.move(dist, theta)
         elif output == bytes(self.return_codes["KMS"], "utf-8"):
             self.ser.timeout = self.move_timeout
-            output = self.ser.read()
+            output = self.serial_read()
             if output == bytes(self.return_codes["RPOOK"], "utf-8"):
                 return True
             elif output == bytes(self.return_codes["RPOUT"], "utf-8"):
@@ -101,19 +109,18 @@ class RobotManager:
 
     def go_angle(self, theta):
         message = self.operation["SRO"] + self.type["int_16"] + "001"
-        logging.info(bytes(message, "utf-8"))
         if self.simulation:
             logging.debug("Going to angle: {}".format(theta))
             return True
-        self.ser.write(message)
-        self.ser.write(bytes(str(theta), "utf-8"))
+        self.serial_write(bytes(message, "utf-8"))
+        self.serial_write(bytes(str(theta), "utf-8"))
         self.ser.timeout = self.ksm_timeout
-        output = self.ser.read()
+        output = self.serial_read()
         if output != bytes(self.return_codes["KMS"], "utf-8"):
             self.go_angle(theta)
         elif output == bytes(self.return_codes["KMS"], "utf-8"):
             self.ser.timeout = self.move_timeout
-            output = self.ser.read()
+            output = self.serial_read()
             if output == bytes(self.return_codes["RROOK"], "utf-8"):
                 return True
             elif output == bytes(self.return_codes["RROOUT"], "utf-8"):
@@ -121,13 +128,12 @@ class RobotManager:
 
     def reset(self):
         message = self.operation["RESET"] + self.type["int_16"] + "000"
-        logging.info(bytes(message, "utf-8"))
         if self.simulation:
             logging.debug("Resetting")
             return True
-        self.ser.write(message)
+        self.serial_write(bytes(message, "utf-8"))
         self.ser.timeout = self.ksm_timeout
-        output = self.ser.read()
+        output = self.serial_read()
         if output != bytes(self.return_codes["KMS"], "utf-8"):
             time.sleep(0.1)
             return True
@@ -136,25 +142,23 @@ class RobotManager:
 
     def go_speed(self):
         message = self.operation["SVI"] + self.type["int_16"] + "000"
-        logging.info(bytes(message, "utf-8"))
         if self.simulation:
             logging.debug("Going to speed")
             return True
-        self.ser.write(bytes(message, "utf-8"))
+        self.serial_write(bytes(message, "utf-8"))
         self.ser.timeout = self.ksm_timeout
-        output = self.ser.read()
+        output = self.serial_read()
         if output != bytes(self.return_codes["KMS"], "utf-8"):
             return True
 
     def stop(self):
         message = self.operation["STOP"] + self.type["int_16"] + "000"
-        logging.info(bytes(message, "utf-8"))
         if self.simulation:
             logging.debug("Stopping")
             return True
-        self.ser.write(message)
+        self.serial_write(bytes(message, "utf-8"))
         self.ser.timeout = self.ksm_timeout
-        output = self.ser.read()
+        output = self.serial_read()
         if output != bytes(self.return_codes["KMS"], "utf-8"):
             return True
         else:
