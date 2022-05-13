971f60e769324c1cfed2e03ed336836bd905189d
MANGIN Lucas <lucasmangin@laposte.net>
2022-05-13 21:25:15 +0200

    Add STM32 serial protocol test project
    - parse header
    - error detection
    - send ack packet ("KMS" (: )

---
+++ pls/Core/Inc/pls.h
@@ -0,0 +1,59 @@
+/*
+ * pls.h
+ *
+ *  Created on: May 10, 2022
+ *      Author: lucas
+ */
+
+#ifndef SRC_PLS_H_
+#define SRC_PLS_H_
+
+#include "stm32f4xx_hal.h"
+
+#define MAX_CMD_BUF 16
+#define MAX_PARAM 2
+
+#define ID_MASK 0xF0
+#define TYPE_MASK 0x08
+#define NCMD_MASK 0x07
+
+/*
+ * 		| 1er octet | 2e octet | 3e octet | 4e octet | 5e octet | ...
+ * 		| en-tete   |         var1        |         var2        | ...
+ * 	ex:	| iiiitnnn  | 0x12     |  0x34    | 0x56       0x78     | ...
+ *
+ * i: identifiant commande (16 possiblité)
+ * t: type des variables (2 max)
+ * n: nombre de variables (8 max)
+ */
+
+// identifiant commande rasp->nucleo
+#define GPO 0x00
+#define SPO 0x01
+#define PLS_RESET 0x02
+#define GRO 0x03
+#define SRO 0x04
+#define SVI 0x05
+#define STOP 0x06
+
+// identifiant commande nucleo->rasp
+#define KMS 0x00  //rx confirmation ("ACK")
+#define RPOOK 0x01
+#define ROUT 0x02
+#define RROOK 0x03
+#define RPOUT 0x04
+#define ERROR 0x05 // something wrong happened :'(
+
+void send_byte(uint8_t ch);
+
+/*
+ * wait incoming bytes from serial huart2
+ * returns:
+ * - command id (GPO, SPO, etc)
+ * - parameters array (distance, theta, ...)
+ * - number of params
+ */
+uint8_t wait_cmd(int16_t params[], uint8_t* param_size);
+
+
+#endif /* SRC_PLS_H_ */
---
+++ pls/Core/Src/pls.c
@@ -0,0 +1,56 @@
+/*
+ * pls.c
+ *
+ *  Created on: May 12, 2022
+ *      Author: lucas
+ */
+
+#include "pls.h"
+#include "usart.h"
+
+UART_HandleTypeDef huart2;
+
+uint8_t cmd_buffer[MAX_CMD_BUF];
+uint8_t header, id, type, n_var;
+
+void send_byte(uint8_t ch) {
+	HAL_UART_Transmit(&huart2, &ch, 1, 0xFFFF);
+}
+
+uint8_t wait_cmd(int16_t params[], uint8_t* param_size) {
+  // receive header
+  HAL_UART_Receive(&huart2, cmd_buffer, 1, HAL_MAX_DELAY);
+
+  // parse header
+  header = cmd_buffer[0];
+  id = (header & ID_MASK) >> 4;
+  type = (header & TYPE_MASK) >> 3;
+  n_var = (header & NCMD_MASK);
+
+  // check header
+  if( n_var <= MAX_PARAM && type <= 1 && id <= 6 ) {
+
+	  // receive parameters
+	  HAL_UART_Receive(&huart2, cmd_buffer+1, (type+1)*n_var, HAL_MAX_DELAY);
+
+	  // parse parameters and update param_size variable
+	  // !!! only int16 for the moment
+	  uint8_t ind = 1; // temp buf index
+	  for (uint8_t i = 0; i < n_var; i++) {
+		  params[i] = (cmd_buffer[ind] << 8) + cmd_buffer[ind + 1];
+		ind += 2;
+	  }
+
+	  *param_size = n_var;
+
+	  // envoie confirmation réception
+	  send_byte(KMS);
+
+	  return id;
+  }
+  else {
+	  //flush serial buffer
+	  __HAL_UART_FLUSH_DRREGISTER(&huart2);
+	  return 0xFF;
+  }
+}
