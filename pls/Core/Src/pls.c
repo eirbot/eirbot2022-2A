ad2da3d228e0e2c408913aa35405f46d87cfd99d
MANGIN Lucas <lucasmangin@laposte.net>
2022-05-14 17:11:51 +0200

    Fix code d'erreur lecture de paramètre

--- pls/Core/Src/pls.c
+++ pls/Core/Src/pls.c
@@ -8,52 +8,55 @@
 #include "pls.h"
 #include "usart.h"
 
-//UART_HandleTypeDef huart2;
+UART_HandleTypeDef huart2;
 
 uint8_t cmd_buffer[MAX_CMD_BUF];
+uint8_t header, id, type, n_var;
 
 void send_byte(uint8_t ch) {
-    HAL_UART_Transmit(&huart2, &ch, 1, 0xFFFF);
+	HAL_UART_Transmit(&huart2, &ch, 1, 0xFFFF);
 }
 
 uint8_t wait_cmd(int16_t params[], uint8_t* param_size) {
-    uint8_t header, id, type, n_var;
-    // receive header
-    if (HAL_UART_Receive(&huart2, cmd_buffer, 1, PLS_TIMEOUT) != HAL_OK) {
-        return 0xFF;
-    }
+  // receive header
+  if( HAL_UART_Receive(&huart2, cmd_buffer, 1, PLS_TIMEOUT) != HAL_OK ) {
+	  return 0xFF;
+  }
 
-    // parse header
-    header = cmd_buffer[0];
-    id = (header & ID_MASK) >> 4;
-    type = (header & TYPE_MASK) >> 3;
-    n_var = (header & NCMD_MASK);
+  // parse header
+  header = cmd_buffer[0];
+  id = (header & ID_MASK) >> 4;
+  type = (header & TYPE_MASK) >> 3;
+  n_var = (header & NCMD_MASK);
 
-    // check header
-    if (n_var <= MAX_PARAM && type <= 1 && id <= 6) {
+  // check header
+  if( n_var <= MAX_PARAM && type <= 1 && id <= 6 ) {
 
-        // receive parameters
-        HAL_UART_Receive(&huart2, cmd_buffer + 1, (type + 1) *  n_var, PLS_TIMEOUT);
+	  // receive parameters
+	  if( HAL_UART_Receive(&huart2, cmd_buffer+1, (type+1)*n_var, PLS_TIMEOUT) != HAL_OK ) {
+		  return 0xFF;
+	  }
 
-        // parse parameters and update param_size variable
-        // !!! only int16 for the moment
-        uint8_t ind = 1; // temp buf index
-        for (uint8_t i = 0; i < n_var; i++) {
-            params[i] = (cmd_buffer[ind] << 8) + cmd_buffer[ind + 1];
-            ind += 2;
-        }
+	  // parse parameters and update param_size variable
+	  // !!! only int16 for the moment
+	  uint8_t ind = 1; // temp buf index
+	  for (uint8_t i = 0; i < n_var; i++) {
+		  params[i] = (cmd_buffer[ind] << 8) + cmd_buffer[ind + 1];
+		ind += 2;
+	  }
 
-        *param_size = n_var;
+	  *param_size = n_var;
 
-        // envoie confirmation réception
-        send_byte(KMS);
+	  // envoie confirmation réception
+	  send_byte(KMS);
 
-        return id;
-    } else {
-        //flush serial buffer
-        __HAL_UART_FLUSH_DRREGISTER(&huart2);
+	  return id;
+  }
+  else {
+	  //flush serial buffer
+	  __HAL_UART_FLUSH_DRREGISTER(&huart2);
 
-        send_byte(ERROR);
-        return 0xFF;
-    }
+	  send_byte(ERROR);
+	  return 0xFF;
+  }
 }
